b6127e655e5db3e8236a056ff1ed09b507c7446a
Dyani Gomez <dyanigomez1@gmail.com>
2025-03-06 22:50:14 -0500

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -21,12 +22,30 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
 
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use(pinoHttp({ logger }));
 
+// Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
